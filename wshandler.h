461634c59e3acc4bfdcf2031da96fc0d75e622ff
Bill Porter <madsci1016@gmail.com>
2017-03-07 21:47:57 -0600

    Fix and clean up StreamView code
    Too tired last night to use enum right, and optimized stream for serial

--- wshandler.h
+++ wshandler.h
@@ -211,27 +211,11 @@ void procT(uint8_t *data, AsyncWebSocketClient *client) {
 #if defined(ESPS_MODE_PIXEL)
             client->binary(pixels.getData(),config.channel_count);
 #elif defined(ESPS_MODE_SERIAL)
-            uint16_t qsize = config.channel_count;
-            uint8_t* bufq = (uint8_t *)(malloc(qsize));
-            uint8_t* bufi = serial.getData();
-            if (bufq){
-              uint16_t i = 0;
-              if( config.serial_type == DMX512){
-                while(i<config.channel_count){
-                  bufq[i]=bufi[1+i++];
-                }
-              }
-              else {
-                while(i<config.channel_count+2){
-                  bufq[i]=bufi[2+i++];
-                }
-                
-              }
-              client->binary(bufq,qsize);
-              free(bufq);
-            }
-            else
-              client->binary(serial.getData(),config.channel_count);
+            if( config.serial_type == SerialType::DMX512)
+                client->binary(&serial.getData()[1],config.channel_count);
+            else 
+                client->binary(&serial.getData()[2],config.channel_count);        
+              
 #endif
             break;
         }
