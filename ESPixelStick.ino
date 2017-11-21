322d7baae43f568575e712321dae31d11803ac97
cinoan <git@cinoan.com>
2017-11-21 08:57:16 +0000

    Set default testing colour

--- ESPixelStick.ino
+++ ESPixelStick.ino
@@ -98,7 +98,7 @@ char m_msg_buffer[MSG_BUFFER_SIZE];
 const char CONFIG_FILE[] = "/config.json";
 
 ESPAsyncE131        e131(10);       // ESPAsyncE131 with X buffers
-testing_t           testing;        // Testing mode
+testing_t           testing = {128,128,128,0,0};        // Testing mode
 config_t            config;         // Current configuration
 uint32_t            *seqError;      // Sequence error tracking for each universe
 uint16_t            uniLast = 1;    // Last Universe to listen for
