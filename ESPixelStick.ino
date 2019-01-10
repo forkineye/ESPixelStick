penfold42/ESPixelBoard

finished new gamma graph - can now live edit gamma and brightness - check for valid keys dsDeviceConfig - http post config upload now enabled for all areas

ESPixelStick.ino
@@ -841,14 +841,18 @@ void dsEffectConfig(JsonObject &json) {
 // De-serialize Device Config
 void dsDeviceConfig(JsonObject &json) {
     // Device
-    config.id = json["device"]["id"].as<String>();
+    if (json.containsKey("device")) {
+        config.id = json["device"]["id"].as<String>();
+    }
 
     // E131
-    config.universe = json["e131"]["universe"];
-    config.universe_limit = json["e131"]["universe_limit"];
-    config.channel_start = json["e131"]["channel_start"];
-    config.channel_count = json["e131"]["channel_count"];
-    config.multicast = json["e131"]["multicast"];
+    if (json.containsKey("e131")) {
+        config.universe = json["e131"]["universe"];
+        config.universe_limit = json["e131"]["universe_limit"];
+        config.channel_start = json["e131"]["channel_start"];
+        config.channel_count = json["e131"]["channel_count"];
+        config.multicast = json["e131"]["multicast"];
+    }
 
     // MQTT
     if (json.containsKey("mqtt")) {
@@ -864,16 +868,20 @@ void dsDeviceConfig(JsonObject &json) {
 
 #if defined(ESPS_MODE_PIXEL)
     // Pixel
-    config.pixel_type = PixelType(static_cast<uint8_t>(json["pixel"]["type"]));
-    config.pixel_color = PixelColor(static_cast<uint8_t>(json["pixel"]["color"]));
-    config.gamma = json["pixel"]["gamma"];
-    config.gammaVal = json["pixel"]["gammaVal"];
-    config.briteVal = json["pixel"]["briteVal"];
+    if (json.containsKey("pixel")) {
+        config.pixel_type = PixelType(static_cast<uint8_t>(json["pixel"]["type"]));
+        config.pixel_color = PixelColor(static_cast<uint8_t>(json["pixel"]["color"]));
+        config.gamma = json["pixel"]["gamma"];
+        config.gammaVal = json["pixel"]["gammaVal"];
+        config.briteVal = json["pixel"]["briteVal"];
+    }
 
 #elif defined(ESPS_MODE_SERIAL)
     // Serial
-    config.serial_type = SerialType(static_cast<uint8_t>(json["serial"]["type"]));
-    config.baudrate = BaudRate(static_cast<uint32_t>(json["serial"]["baudrate"]));
+    if (json.containsKey("serial")) {
+        config.serial_type = SerialType(static_cast<uint8_t>(json["serial"]["type"]));
+        config.baudrate = BaudRate(static_cast<uint32_t>(json["serial"]["baudrate"]));
+    }
 #endif
 
 #if defined(ESPS_SUPPORT_PWM)
@@ -1043,6 +1051,16 @@ void serializeConfig(String &jsonString, bool pretty, bool creds) {
         json.prettyPrintTo(jsonString);
     else
         json.printTo(jsonString);
+}
+
+void dsGammaConfig(JsonObject &json) {
+    if (json.containsKey("pixel")) {
+        config.gamma = json["pixel"]["gamma"];
+        config.gammaVal = json["pixel"]["gammaVal"];
+        config.briteVal = json["pixel"]["briteVal"];
+        pixels.setGamma(config.gamma);
+        updateGammaTable(config.gammaVal, config.briteVal);
+    }
 }
 
 // Save configuration JSON file

forkineye/ESPixelStick

Conflicts:
	ESPixelStick.ino
	html/script.js

ESPixelStick.ino
@@ -777,14 +777,18 @@ void dsEffectConfig(JsonObject &json) {
 // De-serialize Device Config
 void dsDeviceConfig(JsonObject &json) {
     // Device
-    config.id = json["device"]["id"].as<String>();
+    if (json.containsKey("device")) {
+        config.id = json["device"]["id"].as<String>();
+    }
 
     // E131
-    config.universe = json["e131"]["universe"];
-    config.universe_limit = json["e131"]["universe_limit"];
-    config.channel_start = json["e131"]["channel_start"];
-    config.channel_count = json["e131"]["channel_count"];
-    config.multicast = json["e131"]["multicast"];
+    if (json.containsKey("e131")) {
+        config.universe = json["e131"]["universe"];
+        config.universe_limit = json["e131"]["universe_limit"];
+        config.channel_start = json["e131"]["channel_start"];
+        config.channel_count = json["e131"]["channel_count"];
+        config.multicast = json["e131"]["multicast"];
+    }
 
     // MQTT
     if (json.containsKey("mqtt")) {
@@ -802,17 +806,21 @@ void dsDeviceConfig(JsonObject &json) {
 
 #if defined(ESPS_MODE_PIXEL)
     // Pixel
-    config.pixel_type = PixelType(static_cast<uint8_t>(json["pixel"]["type"]));
-    config.pixel_color = PixelColor(static_cast<uint8_t>(json["pixel"]["color"]));
-    config.groupSize = json["pixel"]["groupSize"];
-    config.zigSize = json["pixel"]["zigSize"];
-    config.gammaVal = json["pixel"]["gammaVal"];
-    config.briteVal = json["pixel"]["briteVal"];
+    if (json.containsKey("pixel")) {
+        config.pixel_type = PixelType(static_cast<uint8_t>(json["pixel"]["type"]));
+        config.pixel_color = PixelColor(static_cast<uint8_t>(json["pixel"]["color"]));
+        config.groupSize = json["pixel"]["groupSize"];
+        config.zigSize = json["pixel"]["zigSize"];
+        config.gammaVal = json["pixel"]["gammaVal"];
+        config.briteVal = json["pixel"]["briteVal"];
+    }
 
 #elif defined(ESPS_MODE_SERIAL)
     // Serial
-    config.serial_type = SerialType(static_cast<uint8_t>(json["serial"]["type"]));
-    config.baudrate = BaudRate(static_cast<uint32_t>(json["serial"]["baudrate"]));
+    if (json.containsKey("serial")) {
+        config.serial_type = SerialType(static_cast<uint8_t>(json["serial"]["type"]));
+        config.baudrate = BaudRate(static_cast<uint32_t>(json["serial"]["baudrate"]));
+    }
 #endif
 }
 
@@ -948,6 +956,14 @@ void serializeConfig(String &jsonString, bool pretty, bool creds) {
         json.prettyPrintTo(jsonString);
     else
         json.printTo(jsonString);
+}
+
+void dsGammaConfig(JsonObject &json) {
+    if (json.containsKey("pixel")) {
+        config.gammaVal = json["pixel"]["gammaVal"];
+        config.briteVal = json["pixel"]["briteVal"];
+        updateGammaTable(config.gammaVal, config.briteVal);
+    }
 }
 
 // Save configuration JSON file