cca843d6fdb9918c236b37191559d6783392fb89
weinrich <cobaltblue@gmail.com>
2019-08-12 17:44:49 -0400

    Update FPPDiscovery.cpp
    Fix for defect where an empty (null) hostname in the WiFi class causes an exception. This will happen "out of the box" when the config file hasn't been setup with a hostname and there isn't one specified in the ESPixelStick.ino setup code.
    
    We could default to something in the INO but this is a good change anyway since it will protect folks in case they accidently remove the default from the caller.

--- FPPDiscovery.cpp
+++ FPPDiscovery.cpp
@@ -67,7 +67,9 @@ void FPPDiscovery::sendPingPacket() {
     packet.operatingMode = 0x01; // we only support bridge mode
     uint32_t ip = static_cast<uint32_t>(WiFi.localIP());
     memcpy(packet.ipAddress, &ip, 4);
-    strcpy(packet.hostName, WiFi.hostname().c_str());
+    if (WiFi.hostname()) {
+      strcpy(packet.hostName, WiFi.hostname().c_str());
+    }
     strcpy(packet.version, version);
     strcpy(packet.hardwareType, "ESPixelStick");
     packet.ranges[0] = 0;
