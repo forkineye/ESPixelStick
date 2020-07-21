e83cb768468e31ab6ede68ea40026cf926251ffa
Martin Mueller <MartinMueller2003@yahoo.com>
2020-07-21 13:04:49 -0400

    Removed debug redirect

--- html/script.js
+++ html/script.js
@@ -552,7 +552,7 @@ function wsConnect()
             target = document.location.host;
         }
 
-        target = "192.168.10.99";
+        // target = "192.168.10.99";
         // target = "192.168.10.102";
 
         // Open a new web socket and set the binary type
