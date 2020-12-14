415f8b8405fb861a3aa167ea207b0bba620b41ea
Martin Mueller <MartinMueller2003@yahoo.com>
2020-12-14 11:31:38 -0500

    removed unneeded if statement

--- src/output/OutputMgr.cpp
+++ src/output/OutputMgr.cpp
@@ -633,10 +633,7 @@ bool c_OutputMgr::ProcessJsonConfig (JsonObject& jsonConfig)
             // DEBUG_V ("");
 
             // send the config to the driver. At this level we have no idea what is in it
-            if (false == pOutputChannelDrivers[ChannelIndex]->SetConfig (OutputChannelDriverConfig))
-            {
-
-            }
+            pOutputChannelDrivers[ChannelIndex]->SetConfig (OutputChannelDriverConfig);
 
         } // end for each channel
 
