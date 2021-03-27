3b34d3136703b450d7010dbb7676aae60763cd0b
Martin Mueller <MartinMueller2003@yahoo.com>
2021-03-27 13:27:10 -0400

    Reduced size of I/O selection fields

--- html/script.js
+++ html/script.js
@@ -693,7 +693,7 @@ function CreateOptionsFromConfig(OptionListName, Config)
         {
             // create the selection box
             $('#fg_' + OptionListName).append('<label class="control-label col-sm-2" for="' + OptionListName + ChannelId + '">' + GenerateInputOutputControlName(OptionListName, ChannelId) + ' Mode</label>');
-            $('#fg_' + OptionListName).append('<div class="col-sm-4"><select class="form-control wsopt" id="' + OptionListName + ChannelId + '"></select></div>');
+            $('#fg_' + OptionListName).append('<div class="col-sm-2"><select class="form-control wsopt" id="' + OptionListName + ChannelId + '"></select></div>');
             $('#fg_' + OptionListName + '_mode').append('<fieldset id="' + OptionListName + 'mode' + ChannelId + '"></fieldset>');
         }
 
