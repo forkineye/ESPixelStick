190b9d62e4538b5f69c7b39345294ca581af3014
Martin Mueller <MartinMueller2003@yahoo.com>
2021-09-13 16:24:15 -0400

    Fixed issue with setting idle bits for WS2811 RMT

--- ESPixelStick/src/GPIO_Defs.hpp
+++ ESPixelStick/src/GPIO_Defs.hpp
@@ -95,7 +95,8 @@ typedef enum
 #define DEFAULT_UART_1_GPIO     gpio_num_t::GPIO_NUM_2
 #define DEFAULT_UART_2_GPIO     gpio_num_t::GPIO_NUM_13
 
-#define DEFAULT_RMT_0_GPIO      gpio_num_t::GPIO_NUM_12
+#define DEFAULT_RMT_0_GPIO      gpio_num_t::GPIO_NUM_2
+// #define DEFAULT_RMT_0_GPIO      gpio_num_t::GPIO_NUM_12
 #define DEFAULT_RMT_1_GPIO      gpio_num_t::GPIO_NUM_14
 #define DEFAULT_RMT_2_GPIO      gpio_num_t::GPIO_NUM_32
 #define DEFAULT_RMT_3_GPIO      gpio_num_t::GPIO_NUM_33
