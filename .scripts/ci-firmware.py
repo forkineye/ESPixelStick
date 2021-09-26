82829a7bd41631cff193f4b6789124cb810b3bfa
Shelby Merrick <sporadic@forkineye.com>
2021-09-26 14:14:58 -0400

    Add tagged release support

--- .scripts/ci-firmware.py
+++ .scripts/ci-firmware.py
@@ -8,17 +8,12 @@ with open("dist/firmware/VERSION", "r") as file:
     version = file.readline().rstrip()
     file.close
 
-if RELEASE in os.environ:
-    with open("dist/firmware.json", "r+") as file:
-        data = json.load(file)
+with open("dist/firmware/firmware.json", "r+") as file:
+    data = json.load(file)
+    if RELEASE in os.environ:
         data["release"] = f"ESPixelStick {version}"
-        file.seek(0)
-        json.dump(data, file)
-        file.truncate()
-else:
-    with open(".ci/firmware.json", "r+") as file:
-        data = json.load(file)
-        data["release"] = f"ESPixelStick CI Build #{version}"
-        file.seek(0)
-        json.dump(data, file)
-        file.truncate()
+    else:
+        data["release"] = f"ESPixelStick Build #{version} (untested)"
+    file.seek(0)
+    json.dump(data, file)
+    file.truncate()     
