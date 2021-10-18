ee560d2b6fb4100035f1d278bfd1d71cb4dc84df
Martin Mueller <MartinMueller2003@yahoo.com>
2021-10-17 20:08:15 -0400

    Fixed issue with floating point comparison in json read.

--- ESPixelStick/src/ESPixelStick.h
+++ ESPixelStick/src/ESPixelStick.h
@@ -80,6 +80,24 @@ extern  bool IsBooting;
 extern  bool ResetWiFi;
 static const String ConfigFileName = "/config.json";
 
+template <typename J, typename N>
+bool setFromJSON (float & OutValue, J& Json, N Name)
+{
+    bool HasBeenModified = false;
+
+    if (true == Json.containsKey (Name))
+    {
+        float temp = Json[Name];
+        if (fabs (temp - OutValue) > 0.000005F)
+        {
+            OutValue = temp;
+            HasBeenModified = true;
+        }
+    }
+
+    return HasBeenModified;
+};
+
 template <typename T, typename J, typename N>
 bool setFromJSON (T& OutValue, J& Json, N Name)
 {
