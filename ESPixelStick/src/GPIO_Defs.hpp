0540f8ff68767bddd7ade6994b7395997e5a1b86
Shelby Merrick <sporadic@forkineye.com>
2021-10-21 12:09:58 -0400

    Added missing SD defs for ESP8266

--- ESPixelStick/src/GPIO_Defs.hpp
+++ ESPixelStick/src/GPIO_Defs.hpp
@@ -79,6 +79,9 @@ typedef enum
 #define DEFAULT_UART_1_GPIO     gpio_num_t::GPIO_NUM_2
 
 // File Manager
+#define SD_CARD_MISO_PIN        gpio_num_t::GPIO_NUM_12
+#define SD_CARD_MOSI_PIN        gpio_num_t::GPIO_NUM_13
+#define SD_CARD_CLK_PIN         gpio_num_t::GPIO_NUM_14
 #define SD_CARD_CS_PIN          gpio_num_t::GPIO_NUM_15
 
 #elif defined(ESP32_CAM)
