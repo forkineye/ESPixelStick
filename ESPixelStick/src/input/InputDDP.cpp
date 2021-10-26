66b3744ac05530f025e56a7e665a6749713bdb57
Martin Mueller <MartinMueller2003@yahoo.com>
2021-10-26 09:26:30 -0400

    Added Device ID in the DDP response Removed build date from the response.

--- ESPixelStick/src/input/InputDDP.cpp
+++ ESPixelStick/src/input/InputDDP.cpp
@@ -318,8 +318,9 @@ void c_InputDDP::ProcessReceivedQuery ()
             DynamicJsonDocument JsonConfigDoc (2048);
             JsonObject JsonConfig = JsonConfigDoc.createNestedObject (CN_config);
             JsonConfig[CN_hostname] = config.hostname;
+            JsonConfig[CN_id] = config.id;
             JsonConfig[CN_ip] = WiFi.localIP ().toString ();
-            JsonConfig[CN_version] = (VERSION + String (":") + BUILD_DATE);
+            JsonConfig[CN_version] = VERSION;
             JsonConfig["hardwareType"] = FPP_VARIANT_NAME;
             JsonConfig[CN_type] = FPP_TYPE_ID;
             JsonConfig[CN_num_chan] = InputDataBufferSize;
