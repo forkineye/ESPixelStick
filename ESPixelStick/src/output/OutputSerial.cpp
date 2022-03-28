cc3eec8a4c07e6a800577ec34c330aed3edd83a5
Martin Mueller <MartinMueller2003@yahoo.com>
2022-03-27 21:36:22 -0400

    Turned on Serial Support

---
+++ ESPixelStick/src/output/OutputSerial.cpp
@@ -0,0 +1,431 @@
+/*
+* OutputSerial.cpp - Pixel driver code for ESPixelStick UART
+*
+* Project: ESPixelStick - An ESP8266 / ESP32 and E1.31 based pixel driver
+* Copyright (c) 2015 Shelby Merrick
+* http://www.forkineye.com
+*
+*  This program is provided free for you to use in any way that you wish,
+*  subject to the laws and regulations where you are using it.  Due diligence
+*  is strongly suggested before using this code.  Please give credit where due.
+*
+*  The Author makes no warranty of any kind, express or implied, with regard
+*  to this program or the documentation contained in this document.  The
+*  Author shall not be liable in any event for incidental or consequential
+*  damages in connection with, or arising out of, the furnishing, performance
+*  or use of these programs.
+*
+*/
+
+#include "../ESPixelStick.h"
+#if defined(SUPPORT_OutputType_DMX) || defined(SUPPORT_OutputType_Serial) || defined(SUPPORT_OutputType_Renard)
+
+#include "OutputSerial.hpp"
+#define ADJUST_INTENSITY_AT_ISR
+
+//----------------------------------------------------------------------------
+c_OutputSerial::c_OutputSerial (c_OutputMgr::e_OutputChannelIds OutputChannelId,
+    gpio_num_t outputGpio,
+    uart_port_t uart,
+    c_OutputMgr::e_OutputType outputType) :
+    c_OutputCommon (OutputChannelId, outputGpio, uart, outputType)
+{
+    // DEBUG_START;
+
+    // DEBUG_END;
+} // c_OutputSerial
+
+//----------------------------------------------------------------------------
+c_OutputSerial::~c_OutputSerial ()
+{
+    // DEBUG_START;
+
+    // DEBUG_END;
+} // ~c_OutputSerial
+
+//----------------------------------------------------------------------------
+void c_OutputSerial::GetConfig (ArduinoJson::JsonObject& jsonConfig)
+{
+    // DEBUG_START;
+
+    jsonConfig[CN_gen_ser_hdr] = GenericSerialHeader;
+    jsonConfig[CN_gen_ser_ftr] = GenericSerialFooter;
+    jsonConfig[CN_num_chan]    = Num_Channels;
+    jsonConfig[CN_baudrate]    = CurrentBaudrate;
+
+    c_OutputCommon::GetConfig (jsonConfig);
+
+    // DEBUG_END;
+} // GetConfig
+
+//----------------------------------------------------------------------------
+void c_OutputSerial::GetStatus (ArduinoJson::JsonObject& jsonStatus)
+{
+    // DEBUG_START;
+
+    c_OutputCommon::GetStatus (jsonStatus);
+
+#ifdef USE_SERIAL_DEBUG_COUNTERS
+    JsonObject debugStatus = jsonStatus.createNestedObject("Serial Debug");
+    debugStatus["NextIntensityToSend"] = NextIntensityToSend;
+    debugStatus["IntensityBytesSent"] = IntensityBytesSent;
+    debugStatus["IntensityBytesSentLastFrame"] = IntensityBytesSentLastFrame;
+    debugStatus["FrameStartCounter"] = FrameStartCounter;
+    debugStatus["FrameEndCounter"] = FrameEndCounter;
+    debugStatus["AbortFrameCounter"] = AbortFrameCounter;
+#endif // def USE_SERIAL_DEBUG_COUNTERS
+
+    // DEBUG_END;
+} // GetStatus
+
+//----------------------------------------------------------------------------
+void c_OutputSerial::GetDriverName(String &sDriverName)
+{
+    switch (OutputType)
+    {
+#ifdef SUPPORT_OutputType_Serial
+    case c_OutputMgr::e_OutputType::OutputType_Serial:
+    {
+        sDriverName = F("Serial");
+        break;
+    }
+#endif // def SUPPORT_OutputType_Serial
+
+#ifdef SUPPORT_OutputType_DMX
+    case c_OutputMgr::e_OutputType::OutputType_DMX:
+    {
+        sDriverName = F("DMX");
+        break;
+    }
+#endif // def SUPPORT_OutputType_DMX
+
+#ifdef SUPPORT_OutputType_Renard
+    case c_OutputMgr::e_OutputType::OutputType_Renard:
+    {
+        sDriverName = F("Renard");
+        break;
+    }
+#endif // def SUPPORT_OutputType_Renard
+
+    default:
+    {
+        sDriverName = F("Default");
+        break;
+    }
+    } // switch (OutputType)
+
+} // GetDriverName
+
+//----------------------------------------------------------------------------
+void c_OutputSerial::SetOutputBufferSize(uint16_t NumChannelsAvailable)
+{
+    // DEBUG_START;
+    // DEBUG_V (String ("NumChannelsAvailable: ") + String (NumChannelsAvailable));
+    // DEBUG_V (String ("   GetBufferUsedSize: ") + String (c_OutputCommon::GetBufferUsedSize ()));
+    // DEBUG_V (String ("    OutputBufferSize: ") + String (OutputBufferSize));
+    // DEBUG_V (String ("       BufferAddress: ") + String ((uint32_t)(c_OutputCommon::GetBufferAddress ())));
+
+    do // once
+    {
+        // are we changing size?
+        if (NumChannelsAvailable == OutputBufferSize)
+        {
+            // DEBUG_V ("NO Need to change anything");
+            break;
+        }
+
+        c_OutputCommon::SetOutputBufferSize(NumChannelsAvailable);
+
+        // Calculate our refresh time
+        FrameMinDurationInMicroSec = DMX_US_PER_BIT * DMX_BITS_PER_BYTE * (NumChannelsAvailable + 2);
+        // DEBUG_V (String ("FrameMinDurationInMicroSec: ") + String (FrameMinDurationInMicroSec));
+        // DEBUG_V (String ("      NumChannelsAvailable: ") + String (NumChannelsAvailable));
+    } while (false);
+
+    // DEBUG_END;
+} // SetBufferSize
+
+//----------------------------------------------------------------------------
+bool c_OutputSerial::SetConfig (ArduinoJson::JsonObject& jsonConfig)
+{
+    // DEBUG_START;
+
+    setFromJSON(GenericSerialHeader, jsonConfig, CN_gen_ser_hdr);
+    setFromJSON(GenericSerialFooter, jsonConfig, CN_gen_ser_ftr);
+    setFromJSON(Num_Channels,        jsonConfig, CN_num_chan);
+    setFromJSON(CurrentBaudrate,     jsonConfig, CN_baudrate);
+
+    c_OutputCommon::SetConfig(jsonConfig);
+
+    bool response = validate();
+
+    SerialHeaderSize = GenericSerialHeader.length();
+    SerialFooterSize = GenericSerialFooter.length();
+
+    // Update the config fields in case the validator changed them
+    GetConfig(jsonConfig);
+
+    // DEBUG_END;
+    return response;
+
+} // SetConfig
+
+//----------------------------------------------------------------------------
+bool c_OutputSerial::validate ()
+{
+    // DEBUG_START;
+    bool response = true;
+
+    if ((Num_Channels > MAX_CHANNELS) || (Num_Channels < 1))
+    {
+        logcon(CN_stars + String(F(" Requested channel count was not valid. Setting to ")) + MAX_CHANNELS + " " + CN_stars);
+        Num_Channels = DEFAULT_NUM_CHANNELS;
+        response = false;
+    }
+    SetOutputBufferSize(Num_Channels);
+
+    if ((CurrentBaudrate < uint32_t(BaudRate::BR_MIN)) || (CurrentBaudrate > uint32_t(BaudRate::BR_MAX)))
+    {
+        logcon(CN_stars + String(F(" Requested baudrate is not valid. Setting to Default ")) + CN_stars);
+        CurrentBaudrate = uint32_t(BaudRate::BR_DEF);
+        response = false;
+    }
+
+#if defined(SUPPORT_OutputType_DMX)
+    if (OutputType == c_OutputMgr::e_OutputType::OutputType_DMX)
+    {
+        CurrentBaudrate = uint32_t(BaudRate::BR_DMX);
+    }
+#endif // defined(SUPPORT_OutputType_DMX)
+
+    if (GenericSerialHeader.length() > MAX_HDR_SIZE)
+    {
+        logcon(CN_stars + String(F(" Requested header is too long. Setting to Default ")) + CN_stars);
+        GenericSerialHeader = "";
+    }
+
+    if (GenericSerialFooter.length() > MAX_FOOTER_SIZE)
+    {
+        logcon(CN_stars + String(F(" Requested footer is too long. Setting to Default ")) + CN_stars);
+        GenericSerialFooter = "";
+    }
+
+    // DEBUG_END;
+    return response;
+
+} // validate
+
+//----------------------------------------------------------------------------
+void c_OutputSerial::SetFrameDurration (float IntensityBitTimeInUs)
+{
+    // DEBUG_START;
+
+    size_t TotalBits = NumBitsPerIntensity * Num_Channels;
+
+    FrameMinDurationInMicroSec = (IntensityBitTimeInUs * TotalBits) + InterFrameGapInMicroSec;
+
+    // DEBUG_V (String ("          OutputBufferSize: ") + String (OutputBufferSize));
+    // DEBUG_V (String ("      IntensityBitTimeInUs: ") + String (IntensityBitTimeInUs));
+    // DEBUG_V (String ("   InterFrameGapInMicroSec: ") + String (InterFrameGapInMicroSec));
+
+    // DEBUG_END;
+
+} // SetFrameDurration
+
+//----------------------------------------------------------------------------
+void IRAM_ATTR c_OutputSerial::StartNewFrame ()
+{
+    // DEBUG_START;
+
+#ifdef USE_SERIAL_DEBUG_COUNTERS
+    if (MoreDataToSend ())
+    {
+        AbortFrameCounter++;
+    }
+    FrameStartCounter++;
+#endif // def USE_SERIAL_DEBUG_COUNTERS
+
+    NextIntensityToSend = GetBufferAddress();
+    intensity_count = Num_Channels;
+    SentIntensityCount = 0;
+    SerialHeaderIndex = 0;
+    SerialFooterIndex = 0;
+
+#ifdef USE_SERIAL_DEBUG_COUNTERS
+    IntensityBytesSentLastFrame = IntensityBytesSent;
+    IntensityBytesSent = 0;
+    // IntensityBytesSentLastFrame = 0;
+#endif // def USE_SERIAL_DEBUG_COUNTERS
+
+    // start the next frame
+    switch (OutputType)
+    {
+#ifdef SUPPORT_OutputType_DMX
+        case c_OutputMgr::e_OutputType::OutputType_DMX:
+        {
+            SerialFrameState = SerialFrameState_t::DMXSendFrameStart;
+            break;
+        }  // DMX512
+#endif // def SUPPORT_OutputType_DMX
+
+#ifdef SUPPORT_OutputType_Renard
+        case c_OutputMgr::e_OutputType::OutputType_Renard:
+        {
+            SerialFrameState = SerialFrameState_t::RenardFrameStart;
+            break;
+        }  // RENARD
+#endif // def SUPPORT_OutputType_Renard
+
+#ifdef SUPPORT_OutputType_Serial
+        case c_OutputMgr::e_OutputType::OutputType_Serial:
+        {
+            SerialFrameState = (SerialHeaderSize) ? SerialFrameState_t::GenSerSendHeader : SerialFrameState_t::GenSerSendData;
+        }  // GENERIC
+#endif // def SUPPORT_OutputType_Serial
+
+        default:
+        {
+            break;
+        } // this is not possible but the language needs it here
+
+    } // end switch (OutputType)
+
+    // DEBUG_END;
+
+} // StartNewFrame
+
+//----------------------------------------------------------------------------
+uint8_t IRAM_ATTR c_OutputSerial::GetNextIntensityToSend ()
+{
+    uint8_t data = 0x00;
+
+#ifdef USE_SERIAL_DEBUG_COUNTERS
+    IntensityBytesSent++;
+#endif // def USE_SERIAL_DEBUG_COUNTERS
+
+    switch (SerialFrameState)
+    {
+        case SerialFrameState_t::RenardFrameStart:
+        {
+            data = RenardFrameDefinitions_t::FRAME_START_CHAR;
+            SerialFrameState = SerialFrameState_t::RenardDataStart;
+            break;
+        }
+
+        case SerialFrameState_t::RenardDataStart:
+        {
+            data = RenardFrameDefinitions_t::CMD_DATA_START;
+            SerialFrameState = SerialFrameState_t::RenardSendData;
+            break;
+        }
+
+        case SerialFrameState_t::RenardSendData:
+        {
+            data = *NextIntensityToSend;
+            // do we have to adjust the renard data stream?
+            if ((data >= RenardFrameDefinitions_t::MIN_VAL_TO_ESC) &&
+                (data <= RenardFrameDefinitions_t::MAX_VAL_TO_ESC))
+            {
+                // Send a two byte substitute for the value
+                data = RenardFrameDefinitions_t::ESC_CHAR;
+                SerialFrameState = SerialFrameState_t::RenardSendEscapedData;
+
+            } // end modified data
+            else
+            {
+                ++NextIntensityToSend;
+                if (0 == --intensity_count)
+                {
+                    SerialFrameState = SerialFrameState_t::SerialIdle;
+                }
+            }
+
+            break;
+        }
+
+        case SerialFrameState_t::RenardSendEscapedData:
+        {
+            data = *NextIntensityToSend - uint8_t(RenardFrameDefinitions_t::ESCAPED_OFFSET);
+            ++NextIntensityToSend;           
+            if (0 == --intensity_count)
+            {
+                SerialFrameState = SerialFrameState_t::SerialIdle;
+            }
+            else
+            {
+                SerialFrameState = SerialFrameState_t::RenardSendData;
+            }
+            break;
+        }
+
+        case SerialFrameState_t::DMXSendFrameStart:
+        {
+            data = 0x00; // DMX Lighting frame start
+            SerialFrameState = SerialFrameState_t::DMXSendData;
+            break;
+        }
+
+        case SerialFrameState_t::DMXSendData:
+        {
+            data = *NextIntensityToSend++;
+            if (0 == --intensity_count)
+            {
+                SerialFrameState = SerialFrameState_t::SerialIdle;
+            }
+            break;
+        }
+
+        case SerialFrameState_t::GenSerSendHeader:
+        {
+            data = GenericSerialHeader[SerialHeaderIndex++];
+            if (SerialHeaderSize <= SerialHeaderIndex)
+            {
+                SerialFrameState = SerialFrameState_t::GenSerSendData;
+            }
+            break;
+        }
+
+        case SerialFrameState_t::GenSerSendData:
+        {
+            data = *NextIntensityToSend++;
+            if (0 == --intensity_count)
+            {
+                if (SerialFooterSize)
+                {
+                    SerialFrameState = SerialFrameState_t::GenSerSendFooter;
+                }
+                else
+                {
+                    SerialFrameState = SerialFrameState_t::SerialIdle;
+                }
+            }
+            break;
+        }
+
+        case SerialFrameState_t::GenSerSendFooter:
+        {
+            data = GenericSerialFooter[SerialFooterIndex++];
+            if (SerialFooterSize <= SerialFooterIndex)
+            {
+                SerialFrameState = SerialFrameState_t::SerialIdle;
+            }
+            break;
+        }
+
+        case SerialFrameState_t::SerialIdle:
+        default:
+        {
+            break;
+        }
+    } // switch SerialFrameState
+
+/*
+    if (InvertData)
+    {
+        response = ~response;
+    }
+*/
+    return data;
+} // NextIntensityToSend
+
+#endif // defined(SUPPORT_OutputType_DMX) || defined(SUPPORT_OutputType_Serial) || defined(SUPPORT_OutputType_Renard)
---
+++ ESPixelStick/src/output/OutputSerial.hpp
@@ -0,0 +1,141 @@
+#pragma once
+/*
+* OutputSerial.h - Pixel driver code for ESPixelStick
+*
+* Project: ESPixelStick - An ESP8266 / ESP32 and E1.31 based pixel driver
+* Copyright (c) 2015 Shelby Merrick
+* http://www.forkineye.com
+*
+*  This program is provided free for you to use in any way that you wish,
+*  subject to the laws and regulations where you are using it.  Due diligence
+*  is strongly suggested before using this code.  Please give credit where due.
+*
+*  The Author makes no warranty of any kind, express or implied, with regard
+*  to this program or the documentation contained in this document.  The
+*  Author shall not be liable in any event for incidental or consequential
+*  damages in connection with, or arising out of, the furnishing, performance
+*  or use of these programs.
+*
+*   This is a derived class that converts data in the output buffer into
+*   pixel intensities and then transmits them through the configured serial
+*   interface.
+*
+*/
+
+#include "OutputCommon.hpp"
+#if defined(SUPPORT_OutputType_DMX) || defined(SUPPORT_OutputType_Serial) || defined(SUPPORT_OutputType_Renard)
+
+class c_OutputSerial : public c_OutputCommon
+{
+public:
+    // These functions are inherited from c_OutputCommon
+    c_OutputSerial (c_OutputMgr::e_OutputChannelIds OutputChannelId,
+                   gpio_num_t outputGpio,
+                   uart_port_t uart,
+                   c_OutputMgr::e_OutputType outputType);
+    virtual ~c_OutputSerial ();
+
+    // functions to be provided by the derived class
+    // virtual void         Begin () {};
+    virtual bool         SetConfig (ArduinoJson::JsonObject & jsonConfig); ///< Set a new config in the driver
+    virtual void         GetConfig (ArduinoJson::JsonObject & jsonConfig); ///< Get the current config used by the driver
+            void         GetDriverName (String& sDriverName);
+            c_OutputMgr::e_OutputType GetOutputType ();
+    virtual void         GetStatus (ArduinoJson::JsonObject& jsonStatus);
+            size_t       GetNumChannelsNeeded () { return Num_Channels; };
+            void         SetOutputBufferSize (size_t NumChannelsAvailable);
+            bool         MoreDataToSend () { return (SerialFrameState_t::SerialIdle != SerialFrameState); }
+            void         SetOutputBufferSize(uint16_t NumChannelsAvailable);
+            void         Render() = 0;
+    IRAM_ATTR void       StartNewFrame();
+    IRAM_ATTR uint8_t    GetNextIntensityToSend();
+
+        protected:
+            uint16_t InterFrameGapInMicroSec = 250;
+
+            void SetFrameDurration(float IntensityBitTimeInUs);
+
+#define GS_CHANNEL_LIMIT 2048
+
+    enum class BaudRate
+    {
+        BR_MIN = 38400,
+        BR_DMX = 250000,
+        BR_MAX = 460800,
+        BR_DEF = 57600,
+    };
+
+    uint32_t CurrentBaudrate = uint32_t(BaudRate::BR_DEF); // current transmit rate
+
+    /* DMX minimum timings per E1.11 */
+    const uint8_t   DMX_BREAK            = 92; // 23 bits
+    const uint8_t   DMX_MAB              = 12; //  3 bits
+
+private:
+
+    const size_t    MAX_HDR_SIZE         = 10;    // Max generic serial header size
+    const size_t    MAX_FOOTER_SIZE      = 10; // max generic serial footer size
+    const size_t    MAX_CHANNELS         = 1024;
+    const uint16_t  DEFAULT_NUM_CHANNELS = 64;
+    const size_t    BUF_SIZE             = (MAX_CHANNELS + MAX_HDR_SIZE + MAX_FOOTER_SIZE);
+    const uint32_t  DMX_BITS_PER_BYTE    = (1.0 + 8.0 + 2.0);
+
+#define  DMX_US_PER_BIT uint32_t((1.0 / 250000.0) * 1000000.0)
+
+    uint16_t    Num_Channels = DEFAULT_NUM_CHANNELS;       // Number of data channels to transmit
+
+    uint8_t*    NextIntensityToSend = nullptr;
+    size_t      intensity_count = 0;
+    size_t      SentIntensityCount = 0;
+
+    float       IntensityBitTimeInUs = 0.0;
+    size_t      NumBitsPerIntensity = 1 + 8 + 2;   // Start. 8 Data, Stop
+
+    String      GenericSerialHeader;
+    size_t      SerialHeaderSize  = 0;
+    size_t      SerialHeaderIndex = 0;
+
+    String      GenericSerialFooter;
+    size_t      SerialFooterSize  = 0;
+    size_t      SerialFooterIndex = 0;
+
+#define USE_SERIAL_DEBUG_COUNTERS
+#ifdef USE_SERIAL_DEBUG_COUNTERS
+    size_t     IntensityBytesSent = 0;
+    size_t     IntensityBytesSentLastFrame = 0;
+    uint32_t   FrameStartCounter = 0;
+    uint32_t   FrameEndCounter = 0;
+    uint32_t   AbortFrameCounter = 0;
+#endif // def USE_SERIAL_DEBUG_COUNTERS
+
+    bool validate ();        ///< confirm that the current configuration is valid
+
+    enum RenardFrameDefinitions_t
+    {
+        CMD_DATA_START   = 0x80,
+        ESC_CHAR         = 0x7F,
+        FRAME_START_CHAR = 0x7E,
+        FRAME_PAD_CHAR   = 0x7D,
+        ESCAPED_OFFSET   = 0x4E,
+        MIN_VAL_TO_ESC   = FRAME_PAD_CHAR,
+        MAX_VAL_TO_ESC   = ESC_CHAR
+    };
+
+    enum SerialFrameState_t
+    {
+        RenardFrameStart,
+        RenardDataStart,
+        RenardSendData,
+        RenardSendEscapedData,
+        DMXSendFrameStart,
+        DMXSendData,
+        GenSerSendHeader,
+        GenSerSendData,
+        GenSerSendFooter,
+        SerialIdle
+    };
+    SerialFrameState_t SerialFrameState = SerialIdle;
+
+}; // c_OutputSerial
+
+#endif // defined(SUPPORT_OutputType_DMX) || defined(SUPPORT_OutputType_Serial) || defined(SUPPORT_OutputType_Renard)
