c83e5658bbb527885a3f7cda3993a3b344d2c779
Martin Mueller <MartinMueller2003@yahoo.com>
2022-05-09 16:08:53 -0400

    Cleaned up version

--- .scripts/CopyTargets.py
+++ .scripts/CopyTargets.py
@@ -5,16 +5,17 @@ Import("env")
 
 
 def PrepareDestinationDirectory(DirRoot, DirPath):
-    os.system("ls -al ./")
-    print("mkdirs: Remove path - '" + DirRoot + "'")
-    shutil.rmtree(DirRoot, True)
-    os.system("ls -al ./")
+    # os.system("ls -al ./")
+    # print("mkdirs: Remove path - '" + DirRoot + "'")
+    # shutil.rmtree(DirRoot, True)
+    # os.system("ls -al ./")
     print("mkdirs: path - '" + DirPath + "'")
     os.makedirs(DirPath, 0x777, True)
-    os.system("ls -al ./")
-    print("chmod: " + DirRoot)
-    os.system("chmod -R a+rwx " + DirRoot)
-    os.system("ls -al " + DirPath)
+    # os.system("ls -al ./")
+    # print("chmod: " + DirRoot)
+    if("Windows" != platform.system()):
+        os.system("chmod -R a+rwx " + DirRoot)
+        # os.system("ls -al " + DirPath)
 
 BUILD_DIR = env['PROJECT_BUILD_DIR']
 PIOENV    = env['PIOENV']
@@ -41,29 +42,31 @@ SRC_DBG  = SRC_DIR + PROGNAME + ".elf"
 
 # print("SRC_BIN " + SRC_BIN)
 
-DST_DIR   = "./firmware/" + BOARD_MCU + "/"
+DST_ROOT  = "./firmware/"
+DST_DIR   = DST_ROOT + BOARD_MCU + "/"
 DST_BIN   = DST_DIR + PIOENV + "-app.bin"
 DST_PART  = DST_DIR + PIOENV + "-partitions.bin"
 DST_BOOT  = DST_DIR + PIOENV + "-bootloader.bin"
 
-DBG_DIR   = "./debug/" + BOARD_MCU + "/"
+DBG_ROOT  = "./debug/"
+DBG_DIR   = DBG_ROOT + BOARD_MCU + "/"
 DST_DBG   = DBG_DIR + PIOENV + ".elf"
 
 def after_build(source, target, env):
 
-    pathfrm = os.path.join("", DST_DIR)
-    PrepareDestinationDirectory("./firmware", pathfrm)
+    DstPath = os.path.join("", DST_DIR)
+    PrepareDestinationDirectory(DST_ROOT, DstPath)
     print("Copy from: '" + SRC_BIN + "' to '" + DST_BIN + "'")
     shutil.copyfile(SRC_BIN, DST_BIN)
-    print("Listing dir: " + pathfrm)
-    os.system("ls -al " + pathfrm + "/")
+    # print("Listing dir: " + DstPath)
+    # os.system("ls -al " + DstPath + "/")
 
-    pathdbg = os.path.join("", DBG_DIR)
-    PrepareDestinationDirectory("./debug", pathdbg)
+    DbgPath = os.path.join("", DBG_DIR)
+    PrepareDestinationDirectory(DBG_ROOT, DbgPath)
     print("Copy from: '" + SRC_DBG + "' to '" + DST_DBG + "'")
     shutil.copyfile(SRC_DBG, DST_DBG)
-    print("Listing dir: " + pathdbg)
-    os.system("ls -al " + pathdbg + "/")
+    # print("Listing dir: " + DbgPath)
+    # os.system("ls -al " + DbgPath + "/")
 
     if("FLASH_EXTRA_IMAGES" in env):
         FLASH_EXTRA_IMAGES = env['FLASH_EXTRA_IMAGES']
