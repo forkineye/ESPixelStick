b953ed7b1cbd83f24206976e36e714f510f1d226
Henry Gabryjelski <henrygab@users.noreply.github.com>
2022-05-21 14:53:08 -0700

    Remove final use of `sprintf()`

--- ESPixelStick/src/ESPixelStick.h
+++ ESPixelStick/src/ESPixelStick.h
@@ -114,6 +114,7 @@ bool setFromJSON (T& OutValue, J& Json, N Name)
     return HasBeenModified;
 };
 
+// Safer RGB to HTML string (e.g., "#FF8833") conversion function
 // Template function takes array of characters as **reference**
 // to allow template to statically assert buffer is large enough
 // to always succeed.  Returns esp_err_t to allow use of
@@ -133,6 +134,7 @@ inline esp_err_t saferRgbToHtmlColorString(char (&output)[N], uint8_t r, uint8_t
     // TODO: assert ((wouldHaveWrittenChars > 0) && (wouldHaveWrittenChars < N))
     return ESP_FAIL;
 }
+// Safer seconds to "Minutes:Seconds" string conversion function
 // Template function takes array of characters as **reference**
 // to allow template to statically assert buffer is large enough
 // to always succeed.  Returns esp_err_t to allow use of
@@ -157,6 +159,28 @@ inline esp_err_t saferSecondsToFormattedMinutesAndSecondsString(char (&output)[N
     // TODO: assert ((wouldHaveWrittenChars > 0) && (wouldHaveWrittenChars < N))
     return ESP_FAIL;
 }
+// Safer mac to string (e.g., "00:11:22:33:44:55") string conversion function
+// Template function takes array of characters as **reference**
+// to allow template to statically assert buffer is large enough
+// to always succeed.  Returns esp_err_t to allow use of
+// ESP_ERROR_CHECK() macro (as this is used extensively elsewhere).
+template <size_t N, size_t M>
+inline esp_err_t saferMacAddressToString(char (&output)[N], uint8_t (&mac)[M]) {
+    static_assert(N >= 18); // Including the trailing null, the string requires exactly 18 characters.
+    static_assert(M >=  6); // MAC address input must be at least 6 bytes
+    int wouldHaveWrittenChars =
+        snprintf(
+            output, N,
+            "%02X:%02X:%02X:%02X:%02X:%02X",
+            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]
+            );
+    if (likely((wouldHaveWrittenChars > 0) && (((size_t)wouldHaveWrittenChars) < N))) {
+        return ESP_OK;
+    }
+    // TODO: assert ((wouldHaveWrittenChars > 0) && (wouldHaveWrittenChars < N))
+    return ESP_FAIL;
+}
+
 
 #define logcon(msg) \
 { \
--- ESPixelStick/src/network/ETH_m.cpp
+++ ESPixelStick/src/network/ETH_m.cpp
@@ -636,7 +636,7 @@ String ETHClass::macAddress(void)
     uint8_t mac[6] = {0,0,0,0,0,0};
     char macStr[18] = { 0 };
     macAddress(mac);
-    sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    ESP_ERROR_CHECK(saferMacAddressToString(macStr, mac));
     return String(macStr);
 }
 
