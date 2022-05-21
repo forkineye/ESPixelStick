d2d0851674a6dd5b0180a97013bee78008d38282
Henry Gabryjelski <henrygab@users.noreply.github.com>
2022-05-21 13:36:01 -0700

    Compatibility for ESP8266

--- ESPixelStick/src/backported.h
+++ ESPixelStick/src/backported.h
@@ -48,13 +48,6 @@ typedef int esp_err_t; // currently only need two defined status: OK & FAIL
     #define ESP_ERROR_CHECK(x)                                          \
         do {                                                            \
             esp_err_t err_rc_ = (x);                                    \
-            if (unlikely(err_rc_ != ESP_OK)) {                          \
-                /* TODO: log error */                                   \
-            }                                                           \
-        } while(0)
-    #define ESP_ERROR_CHECK(x)                                          \
-        do {                                                            \
-            esp_err_t err_rc_ = (x);                                    \
             if (err_rc_ != ESP_OK) {                                    \
                 LOG_ERROR_MSG("err: esp_err_t = %d", rc);               \
                 assert(0 && #x);                                        \
