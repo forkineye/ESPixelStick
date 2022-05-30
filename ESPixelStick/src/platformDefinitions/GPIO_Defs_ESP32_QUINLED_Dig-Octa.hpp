57894f0b45f925ccad81f1b6e053f99ad450488a
Martin Mueller <MartinMueller2003@yahoo.com>
2022-05-30 19:14:20 -0400

    Fixed some typos

--- ESPixelStick/src/platformDefinitions/GPIO_Defs_ESP32_QUINLED_Dig-Octa.hpp
+++ ESPixelStick/src/platformDefinitions/GPIO_Defs_ESP32_QUINLED_Dig-Octa.hpp
@@ -3,7 +3,7 @@
  * GPIO_Defs_ESP32_QUINLED_QUAD.hpp - Output Management class
  *
  * Project: ESPixelStick - An ESP8266 / ESP32 and E1.31 based pixel driver
- * Copyright (c) 2021 Shelby Merrick
+ * Copyright (c) 2021 - 2022 Shelby Merrick
  * http://www.forkineye.com
  *
  *  This program is provided free for you to use in any way that you wish,
@@ -46,9 +46,7 @@
 #define SD_CARD_CLK_PIN         gpio_num_t::GPIO_NUM_14
 #define SD_CARD_CS_PIN          gpio_num_t::GPIO_NUM_16
 
-
-// #include <ETH.h>
-#include "../network/ETH_m.h"
+#include <ETH.h>
 
 /*
    * ETH_CLOCK_GPIO0_IN   - default: external clock from crystal oscillator
@@ -59,7 +57,7 @@
 #define DEFAULT_ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
 
 // Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
-#define DEFAULT_ETH_POWER_PIN          gpio_num_t::-1
+#define DEFAULT_ETH_POWER_PIN          gpio_num_t(-1)
 #define DEFAULT_ETH_POWER_PIN_ACTIVE   HIGH
 
 // Type of the Ethernet PHY (LAN8720 or TLK110)
@@ -79,15 +77,14 @@
 #define DEFAULT_ETH_MDC_PIN     gpio_num_t::GPIO_NUM_23
 #define DEFAULT_ETH_MDIO_PIN    gpio_num_t::GPIO_NUM_18
 
-
 // Output Types
 // Not Finished - #define SUPPORT_OutputType_TLS3001
 // #define SUPPORT_OutputType_APA102           // SPI
-// #define SUPPORT_OutputType_DMX              // UART
-// #define SUPPORT_OutputType_GECE             // UART
+// #define SUPPORT_OutputType_DMX              // UART / RMT
+// #define SUPPORT_OutputType_GECE             // UART / RMT
 #define SUPPORT_OutputType_GS8208           // UART / RMT
-// #define SUPPORT_OutputType_Renard           // UART
-// #define SUPPORT_OutputType_Serial           // UART
+// #define SUPPORT_OutputType_Renard           // UART / RMT
+// #define SUPPORT_OutputType_Serial           // UART / RMT
 #define SUPPORT_OutputType_TM1814           // UART / RMT
 #define SUPPORT_OutputType_UCS1903          // UART / RMT
 #define SUPPORT_OutputType_UCS8903          // UART / RMT
