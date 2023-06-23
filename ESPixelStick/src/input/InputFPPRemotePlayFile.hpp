a7dbd45c9d7c8e533409e7efd7964fae71c16f49
Martin <MartinMueller2003@yahoo.com>
2023-06-23 17:34:22 -0400

    Increased FPP Task Stack size to handle new functionality

--- ESPixelStick/src/input/InputFPPRemotePlayFile.hpp
+++ ESPixelStick/src/input/InputFPPRemotePlayFile.hpp
@@ -110,7 +110,7 @@ private:
 
 #ifdef ARDUINO_ARCH_ESP32
     TaskHandle_t TimerPollTaskHandle = NULL;
-#   define TimerPollHandlerTaskStack 2000
+#   define TimerPollHandlerTaskStack 3000
 // #   define TimerPollHandlerTaskStack 6000
 #endif // def ARDUINO_ARCH_ESP32
 
