a1df1e345550db1175e748158e5ca130d9843e7a
Martin <MartinMueller2003@yahoo.com>
2024-03-29 19:42:41 -0400

    Turned off debug output

--- ESPixelStick/src/WebMgr.cpp
+++ ESPixelStick/src/WebMgr.cpp
@@ -736,7 +736,7 @@ void c_WebMgr::FirmwareUpload (AsyncWebServerRequest* request,
         if (efupdate.hasError ())
         {
             logcon (String(CN_stars) + F (" UPDATE ERROR: ") + String (efupdate.getError ()));
-            DEBUG_V ("efupdate.hasError");
+            // DEBUG_V ("efupdate.hasError");
             request->send (200, CN_textSLASHplain, (String (F ("Update Error: ")) + String (efupdate.getError ()).c_str()));
             break;
         }
