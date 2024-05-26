e110b8a5c017f93982fde803d0fde6ee60874048
noman1228 <jay.m.theriault@gmail.com>
2024-05-25 20:49:55 -0400

    Requested changes

--- ESPixelStick/src/platformDefinitions/GPIO_Defs_ESP32_DevkitC_v4_OLED.hpp
+++ ESPixelStick/src/platformDefinitions/GPIO_Defs_ESP32_DevkitC_v4_OLED.hpp
@@ -19,15 +19,16 @@
  */
 
 //Output Manager
-#define DEFAULT_RMT_0_GPIO      gpio_num_t::GPIO_NUM_25
-#define DEFAULT_RMT_1_GPIO      gpio_num_t::GPIO_NUM_26
+#define DEFAULT_RMT_0_GPIO     gpio_num_t::GPIO_NUM_25
+#define DEFAULT_RMT_1_GPIO     gpio_num_t::GPIO_NUM_26
+
 #define DEFAULT_RMT_2_GPIO      gpio_num_t::GPIO_NUM_27
-#define DEFAULT_RMT_3_GPIO      gpio_num_t::GPIO_NUM_14
+#define DEFAULT_RMT_3_GPIO      gpio_num_t::GPIO_NUM_31
 
 // SPI Output
 //#define SUPPORT_SPI_OUTPUT
 #define DEFAULT_SPI_DATA_GPIO  gpio_num_t::GPIO_NUM_15
-#define DEFAULT_SPI_CLOCK_GPIO gpio_num_t::GPIO_NUM_25
+#define DEFAULT_SPI_CLOCK_GPIO gpio_num_t::GPIO_NUM_17
 
 #define DEFAULT_I2C_SDA        gpio_num_t::GPIO_NUM_21
 #define DEFAULT_I2C_SCL        gpio_num_t::GPIO_NUM_22
@@ -42,17 +43,17 @@
 // Output Types
 // Not Finished - #define SUPPORT_OutputType_TLS3001
 //#define SUPPORT_OutputType_APA102           // SPI
-#define SUPPORT_OutputType_DMX              // UART
-#define SUPPORT_OutputType_GECE             // UART
-#define SUPPORT_OutputType_GS8208           // UART / RMT
-#define SUPPORT_OutputType_Renard           // UART
-#define SUPPORT_OutputType_Serial           // UART
-#define SUPPORT_OutputType_TM1814           // UART / RMT
-#define SUPPORT_OutputType_UCS1903          // UART / RMT
-#define SUPPORT_OutputType_UCS8903          // UART / RMT
+//#define SUPPORT_OutputType_DMX              // UART
+//#define SUPPORT_OutputType_GECE             // UART
+//#define SUPPORT_OutputType_GS8208           // UART / RMT
+//#define SUPPORT_OutputType_Renard           // UART
+//#define SUPPORT_OutputType_Serial           // UART
+//#define SUPPORT_OutputType_TM1814           // UART / RMT
+//#define SUPPORT_OutputType_UCS1903          // UART / RMT
+//#define SUPPORT_OutputType_UCS8903          // UART / RMT
 //#define SUPPORT_OutputType_WS2801           // SPI
 #define SUPPORT_OutputType_WS2811           // UART / RMT
-#define SUPPORT_OutputType_Relay            // GPIO
+//#define SUPPORT_OutputType_Relay            // GPIO
 //#define SUPPORT_OutputType_Servo_PCA9685    // I2C (default pins)
 
 
