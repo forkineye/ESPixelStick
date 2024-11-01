c4d753cbbb9801c2686e1ce832d77101c86d3be2
noman1228 <jay.m.theriault@gmail.com>
2024-11-01 12:13:55 -0400

    More trickery

--- ESPixelStick/src/platformDefinitions/GPIO_Defs_ESP32_DevkitC_v4_OLED.hpp
+++ ESPixelStick/src/platformDefinitions/GPIO_Defs_ESP32_DevkitC_v4_OLED.hpp
@@ -18,20 +18,23 @@
  *
  */
 
-//Output Manager
-#define DEFAULT_RMT_0_GPIO     gpio_num_t::GPIO_NUM_25
-#define DEFAULT_RMT_1_GPIO     gpio_num_t::GPIO_NUM_26
+#define DEFAULT_RMT_0_GPIO     gpio_num_t::GPIO_NUM_2
+#define DEFAULT_RMT_1_GPIO     gpio_num_t::GPIO_NUM_13
 
-#define DEFAULT_RMT_2_GPIO      gpio_num_t::GPIO_NUM_27
-#define DEFAULT_RMT_3_GPIO      gpio_num_t::GPIO_NUM_31
+#define DEFAULT_RMT_2_GPIO      gpio_num_t::GPIO_NUM_12
+#define DEFAULT_RMT_3_GPIO      gpio_num_t::GPIO_NUM_14
+#define DEFAULT_RMT_4_GPIO      gpio_num_t::GPIO_NUM_32
+#define DEFAULT_RMT_5_GPIO      gpio_num_t::GPIO_NUM_33
 
 // SPI Output
-//#define SUPPORT_SPI_OUTPUT
-#define DEFAULT_SPI_DATA_GPIO  gpio_num_t::GPIO_NUM_15
-#define DEFAULT_SPI_CLOCK_GPIO gpio_num_t::GPIO_NUM_17
+#define SUPPORT_SPI_OUTPUT
+#define DEFAULT_SPI_DATA_GPIO   gpio_num_t::GPIO_NUM_16
+#define DEFAULT_SPI_CLOCK_GPIO  gpio_num_t::GPIO_NUM_17
+#define DEFAULT_SPI_CS_GPIO     gpio_num_t::GPIO_NUM_0
+#define DEFAULT_SPI_DEVICE      VSPI_HOST
 
-#define DEFAULT_I2C_SDA        gpio_num_t::GPIO_NUM_21
-#define DEFAULT_I2C_SCL        gpio_num_t::GPIO_NUM_22
+#define DEFAULT_I2C_SDA         gpio_num_t::GPIO_NUM_3
+#define DEFAULT_I2C_SCL         gpio_num_t::GPIO_NUM_5
 
 // File Manager
 #define SUPPORT_SD
@@ -40,22 +43,24 @@
 #define SD_CARD_CLK_PIN         gpio_num_t::GPIO_NUM_18
 #define SD_CARD_CS_PIN          gpio_num_t::GPIO_NUM_5
 
+#define DEFAULT_RELAY_GPIO      gpio_num_t::GPIO_NUM_1
+
 // Output Types
 // Not Finished - #define SUPPORT_OutputType_TLS3001
-//#define SUPPORT_OutputType_APA102           // SPI
-//#define SUPPORT_OutputType_DMX              // UART
-//#define SUPPORT_OutputType_GECE             // UART
-//#define SUPPORT_OutputType_GS8208           // UART / RMT
-//#define SUPPORT_OutputType_Renard           // UART
-//#define SUPPORT_OutputType_Serial           // UART
-//#define SUPPORT_OutputType_TM1814           // UART / RMT
-//#define SUPPORT_OutputType_UCS1903          // UART / RMT
-//#define SUPPORT_OutputType_UCS8903          // UART / RMT
-//#define SUPPORT_OutputType_WS2801           // SPI
+#define SUPPORT_OutputType_APA102           // SPI
+#define SUPPORT_OutputType_DMX              // UART / RMT
+#define SUPPORT_OutputType_GECE             // UART
+#define SUPPORT_OutputType_GRINCH           // SPI
+#define SUPPORT_OutputType_GS8208           // UART / RMT
+#define SUPPORT_OutputType_Renard           // UART / RMT
+#define SUPPORT_OutputType_Serial           // UART / RMT
+#define SUPPORT_OutputType_TM1814           // UART / RMT
+#define SUPPORT_OutputType_UCS1903          // UART / RMT
+#define SUPPORT_OutputType_UCS8903          // UART / RMT
+#define SUPPORT_OutputType_WS2801           // SPI
 #define SUPPORT_OutputType_WS2811           // UART / RMT
-//#define SUPPORT_OutputType_Relay            // GPIO
-//#define SUPPORT_OutputType_Servo_PCA9685    // I2C (default pins)
-
+#define SUPPORT_OutputType_Relay            // GPIO
+#define SUPPORT_OutputType_Servo_PCA9685    // I2C (default pins)
 
 // OLED SUPPORT
 #define USE_OLED
