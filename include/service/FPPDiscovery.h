01cdb7ce086cca3f2af5c90204212a52ffe27445
Martin <MartinMueller2003@yahoo.com>
2025-02-08 11:20:25 -0500

    Minor refactoring to support a variable configuration record rather then separate config calls.

--- include/service/FPPDiscovery.h
+++ include/service/FPPDiscovery.h
@@ -51,6 +51,8 @@ private:
     bool IsEnabled = false;
     bool BlankOnStop = false;
     bool StopInProgress = false;
+    bool FppSyncOverride = false;
+    String  ConfiguredFileToPlay;
     String UploadFileName;
     IPAddress FppRemoteIp = IPAddress (uint32_t(0));
     c_InputFPPRemotePlayFile * InputFPPRemotePlayFile = nullptr;
@@ -115,26 +117,27 @@ public:
 
     void begin ();
 
-    void ProcessFPPJson     (AsyncWebServerRequest* request);
-    void ProcessFPPDJson    (AsyncWebServerRequest* request);
-    void ProcessGET         (AsyncWebServerRequest* request);
-    void ProcessPOST        (AsyncWebServerRequest* request);
-    void ProcessFile        (AsyncWebServerRequest* request, String filename, uint32_t index, uint8_t* data, uint32_t len, bool final, uint32_t contentLength = 0);
-    void ProcessBody        (AsyncWebServerRequest* request, uint8_t* data, uint32_t len, uint32_t index, uint32_t total);
-    void sendPingPacket     (IPAddress destination = IPAddress(255, 255, 255, 255));
-    void PlayFile           (String & FileToPlay);
-    void Enable             (void);
-    void Disable            (void);
-    void GetStatus          (JsonObject& jsonStatus);
+    void ProcessFPPJson      (AsyncWebServerRequest* request);
+    void ProcessFPPDJson     (AsyncWebServerRequest* request);
+    void ProcessGET          (AsyncWebServerRequest* request);
+    void ProcessPOST         (AsyncWebServerRequest* request);
+    void ProcessFile         (AsyncWebServerRequest* request, String filename, uint32_t index, uint8_t* data, uint32_t len, bool final, uint32_t contentLength = 0);
+    void ProcessBody         (AsyncWebServerRequest* request, uint8_t* data, uint32_t len, uint32_t index, uint32_t total);
+    void sendPingPacket      (IPAddress destination = IPAddress(255, 255, 255, 255));
+    void PlayFile            (String & FileToPlay);
+    void Enable              (void);
+    void Disable             (void);
+    void GetStatus           (JsonObject& jsonStatus);
     void NetworkStateChanged (bool NewNetworkState);
     void SetOperationalState (bool ActiveFlag);
+    bool SetConfig           (JsonObject& jsonConfig);
+    void GetConfig           (JsonObject& jsonConfig);
 
-    void SetBlankOnStop               (bool value) {BlankOnStop = value;}
-    bool GetBlankOnStop               (void) {return BlankOnStop;}
     void SetInputFPPRemotePlayFile    (c_InputFPPRemotePlayFile * value);
     void ForgetInputFPPRemotePlayFile ();
     void GenerateFppSyncMsg           (uint8_t Action, const String & FileName, uint32_t CurrentFrame, const float & ElpsedTime);
     void GetSysInfoJSON               (JsonObject& jsonResponse);
+    bool PlayingAfile                 () {return PlayingFile();}
 
 #   define SYNC_PKT_START       0
 #   define SYNC_PKT_STOP        1
@@ -142,5 +145,6 @@ public:
 #   define SYNC_PKT_OPEN        3
 
 };
+#define No_LocalFileToPlay F("...")
 
 extern c_FPPDiscovery FPPDiscovery;
