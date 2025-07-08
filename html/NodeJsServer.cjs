eedae04b64eb585f5e1406778b15a156faa0f608
Martin <MartinMueller2003@yahoo.com>
2025-07-08 15:29:05 -0400

    Moved port to remove conflict with another app

--- html/NodeJsServer.cjs
+++ html/NodeJsServer.cjs
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(8000, () => {
-    console.log("Application started and Listening on port 8000");
+app.listen(9000, () => {
+    console.log("Application started and Listening on port 9000");
 });
 
 app.use(express.static(__dirname));
