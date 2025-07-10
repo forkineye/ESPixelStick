0ca75201dd37d8ce83a0a1309a442898970109ad
Martin <MartinMueller2003@yahoo.com>
2025-07-10 11:16:29 -0400

    Support debugging ESPS UI without modifying the target in script.js

--- html/NodeJsServer.cjs
+++ html/NodeJsServer.cjs
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const espsIp = "http://192.168.10.235/";
 
 app.listen(9000, () => {
     console.log("Application started and Listening on port 9000");
@@ -10,3 +11,9 @@ app.use(express.static(__dirname));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/XP",        (req, res) => {res.redirect(espsIp + "XP")});
+app.get("/XJ",         (req, res) => {res.redirect(espsIp + "XJ")});
+app.put("/conf/:file", (req, res) => {res.redirect(espsIp + "conf/" + req.params.file);});
+app.get("/conf/:file", (req, res) => {res.redirect(espsIp + "conf/" + req.params.file);});
+app.post("/settime",   (req, res) => {res.redirect(espsIp + "settime?newtime=" + req.query.newtime);});
